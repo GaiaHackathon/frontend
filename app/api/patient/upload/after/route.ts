c9371e2c4913944261b6b058abeded8f37e0d457
Orlando Ortega <orlandoortegajr@gmail.com>
2024-12-11 22:33:02 -0500

    finished after image upload

--- app/api/patient/upload/after/route.ts
+++ app/api/patient/upload/after/route.ts
@@ -1,45 +1,41 @@
-// // Import everything needed for nextjs api route
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { PrismaClient } from '@prisma/client'
+import * as Client from '@web3-storage/w3up-client';
+import * as Proof from '@web3-storage/w3up-client/proof';
+import { Signer } from '@web3-storage/w3up-client/principal/ed25519';
+import { StoreMemory } from '@web3-storage/w3up-client/stores/memory';
+import prisma from '@/db';
 
+export async function POST(request: Request) {
+  const formData = await request.formData();
 
-// interface Image {
-//     imageid:number;
-//     beforeImageCid: string;
-//     afterImageCid: string;
-//     patientId: number;
-// }
+  try {
+    const principal = Signer.parse(process.env.WEB3_STORAGE_KEY!);
+    const store = new StoreMemory();
+    const client = await Client.create({ principal, store });
+    const proof = await Proof.parse(process.env.WEB3_STORAGE_PROOF!);
+    const space = await client.addSpace(proof);
+    await client.setCurrentSpace(space.did());
+    const cid = await client.uploadFile(formData.get('image') as File);
 
-// // create post route
-// export default async function handler(
-//     req: NextApiRequest,
-//     res: NextApiResponse
-// ) {
+    const createdImage = await prisma.image.update({
+      where: {
+        imageid: Number(formData.get('imageid')),
+      },
+      data: {
+        afterImageCid: cid.toString(),
+        afterImageUploaded: true,
+        patientid: Number(formData.get('patientid')),
+      },
+    });
 
-//     if (req.method === 'POST') {
-
-//         const data: Image = req.body;
-
-//         if (!data || !data.beforeImageCid || !data.patientId || !data.afterImageCid || !data.imageid) {
-//             return res.status(400).json({ message: 'Missing required fields' });
-//         }
-//         // create prisma client
-//         const prisma = new PrismaClient()
-
-//         // Get the image according to the imageid and the beforeImageCid and update the afterImageCid
-//         const image = await prisma.image.update({
-//             where: {
-//                 imageid: data.imageid,
-//                 beforeImageCid: data.beforeImageCid,
-//             },
-//             data: {
-//                 afterImageCid: data.afterImageCid,
-//                 afterImageUploaded: true,
-//             },
-//         })
-//         res.status(200).json(image)
-//     }
-//     else {
-//         res.status(400).json({ message: 'Invalid request method' })
-//     }
-// }
+    return Response.json({
+      imageUrl: `https://${cid.toString()}.ipfs.w3s.link`,
+      createdImage,
+    });
+  } catch (err) {
+    console.error(err);
+    return Response.json(
+      { message: 'Error trying to store image in ipfs' },
+      { status: 500 }
+    );
+  }
+}
