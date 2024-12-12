00566acce65ee92461047cf14bfad0e671397eaf
Orlando Ortega <orlandoortegajr@gmail.com>
2024-12-11 22:28:50 -0500

    removed unused code

--- app/api/patient/upload/before/route.ts
+++ app/api/patient/upload/before/route.ts
@@ -34,26 +34,4 @@ export async function POST(request: Request) {
       { status: 500 }
     );
   }
-
-  //   if (req.method === 'POST') {
-  //     const data: Image = req.body;
-
-  //     if (!data || !data.beforeImageCid || !data.patientId) {
-  //       return res.status(400).json({ message: 'Missing required fields' });
-  //     }
-  //     // create prisma client
-  //     const prisma = new PrismaClient();
-
-  //     // create patient
-  //     const patient = await prisma.image.create({
-  //       data: {
-  //         beforeImageCid: data.beforeImageCid,
-  //         patientid: data.patientId,
-  //         afterImageUploaded: false,
-  //       },
-  //     });
-  //     res.status(200).json(patient);
-  //   } else {
-  //     res.status(400).json({ message: 'Invalid request method' });
-  //   }
 }
