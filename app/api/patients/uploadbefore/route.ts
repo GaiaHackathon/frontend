dd0a093cfc0433b48f3e8250d1ff7859d7c2c700
Orlando Ortega <orlandoortegajr@gmail.com>
2024-12-11 20:32:24 -0500

    can now create practitioners

--- app/api/patients/uploadbefore/route.ts
+++ app/api/patients/uploadbefore/route.ts
@@ -1,40 +1,40 @@
-// Import everything needed for nextjs api route
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
+// // Import everything needed for nextjs api route
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { PrismaClient } from '@prisma/client'
 
 
-interface Image {
-    beforeImageCid: string;
-    patientId: number;
-}
+// interface Image {
+//     beforeImageCid: string;
+//     patientId: number;
+// }
 
-// create post route
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
+// // create post route
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse
+// ) {
 
-    if (req.method === 'POST') {
+//     if (req.method === 'POST') {
 
-        const data: Image = req.body;
+//         const data: Image = req.body;
 
-        if (!data || !data.beforeImageCid || !data.patientId) {
-            return res.status(400).json({ message: 'Missing required fields' });
-        }
-        // create prisma client
-        const prisma = new PrismaClient()
+//         if (!data || !data.beforeImageCid || !data.patientId) {
+//             return res.status(400).json({ message: 'Missing required fields' });
+//         }
+//         // create prisma client
+//         const prisma = new PrismaClient()
 
-        // create patient
-        const patient = await prisma.image.create({
-            data: {
-                beforeImageCid: data.beforeImageCid,
-                patientid: data.patientId,
-                afterImageUploaded: false,
-            },
-        })
-        res.status(200).json(patient)
-    }
-    else {
-        res.status(400).json({ message: 'Invalid request method' })
-    }
-}
+//         // create patient
+//         const patient = await prisma.image.create({
+//             data: {
+//                 beforeImageCid: data.beforeImageCid,
+//                 patientid: data.patientId,
+//                 afterImageUploaded: false,
+//             },
+//         })
+//         res.status(200).json(patient)
+//     }
+//     else {
+//         res.status(400).json({ message: 'Invalid request method' })
+//     }
+// }
---
+++ app/api/practitioner/register/route.ts
@@ -0,0 +1,34 @@
+import prisma from '@/db';
+
+type Practitioner = {
+  baseaddress: string;
+  name: string;
+};
+
+export async function POST(request: Request) {
+  const res: Practitioner = await request.json();
+
+  if (!res.baseaddress || !res.name) {
+    return Response.json(
+      { message: 'Missing required fields' },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const patient = await prisma.practitioner.create({
+      data: {
+        baseaddress: res.baseaddress,
+        name: res.name,
+      },
+    });
+
+    return Response.json({ patient });
+  } catch (err) {
+    console.log(err);
+    return Response.json(
+      { message: 'Ran into error creating new patient' },
+      { status: 500 }
+    );
+  }
+}
